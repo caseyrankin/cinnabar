c44afce57b47d7cb03cedb00a045b15fe03b07ff
Chris Dahms <zentrification@gmail.com>
2014-08-22 17:18:59 -0400

    stripped unneeded nokogiri html formatting

--- app/helpers/cinnabar/application_helper.rb
+++ app/helpers/cinnabar/application_helper.rb
@@ -19,13 +19,6 @@ module Cinnabar
       # use html inside block if record does not exist
       content = capture(&block).html_safe if block_given? && content.nil?
 
-      # transform = <<-eos
-      #   <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"> <xsl:output method="xml" encoding="ISO-8859-1"/> <xsl:param name="indent-increment" select="'   '"/> <xsl:template name="newline"> <xsl:text disable-output-escaping="yes"> </xsl:text> </xsl:template> <xsl:template match="comment() | processing-instruction()"> <xsl:param name="indent" select="''"/> <xsl:call-template name="newline"/> <xsl:value-of select="$indent"/> <xsl:copy /> </xsl:template> <xsl:template match="text()"> <xsl:param name="indent" select="''"/> <xsl:call-template name="newline"/> <xsl:value-of select="$indent"/> <xsl:value-of select="normalize-space(.)"/> </xsl:template> <xsl:template match="text()[normalize-space(.)='']"/> <xsl:template match="*"> <xsl:param name="indent" select="''"/> <xsl:call-template name="newline"/> <xsl:value-of select="$indent"/> <xsl:choose> <xsl:when test="count(child::*) > 0"> <xsl:copy> <xsl:copy-of select="@*"/> <xsl:apply-templates select="*|text()"> <xsl:with-param name="indent" select="concat ($indent, $indent-increment)"/> </xsl:apply-templates> <xsl:call-template name="newline"/> <xsl:value-of select="$indent"/> </xsl:copy> </xsl:when> <xsl:otherwise> <xsl:copy-of select="."/> </xsl:otherwise> </xsl:choose> </xsl:template> </xsl:stylesheet>"
-      # eos
-      # xsl = Nokogiri::XSLT(transform)
-      # html = Nokogiri(content)
-      # content = xsl.apply_to(html).to_s.html_safe
-
       content_tag(options[:tag].to_sym, content, data: { mercury: options[:mercury_type] }, id: db_name)
     end
 
--- cinnabar.gemspec
+++ cinnabar.gemspec
@@ -19,7 +19,6 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 3.2.14"
   s.add_dependency "mercury-rails", "~> 0.9.0"
   s.add_dependency "codemirror-rails"
-  #s.add_dependency "nokogiri"
 
   s.add_development_dependency "sqlite3"
 end
